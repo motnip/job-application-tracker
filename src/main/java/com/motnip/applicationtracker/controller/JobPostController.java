3d5798cf40b688b1c77fea472cd85e13c31e24f2
motnip <>
2025-05-28 16:58:26 +0200

    Implement controller with new application service

--- src/main/java/com/motnip/applicationtracker/controller/JobPostController.java
+++ src/main/java/com/motnip/applicationtracker/controller/JobPostController.java
@@ -1,40 +1,62 @@
 package com.motnip.applicationtracker.controller;
 
-import com.motnip.applicationtracker.controller.request.ApplicationAdvancementUpdateRequest;
 import com.motnip.applicationtracker.controller.request.ApplicationFirstContactRequest;
-import com.motnip.applicationtracker.model.Application;
+import com.motnip.applicationtracker.controller.request.ApplicationProgressUpdateRequest;
 import com.motnip.applicationtracker.controller.request.ApplicationRequest;
+import com.motnip.applicationtracker.controller.response.ApplicationResponse;
+import com.motnip.applicationtracker.controller.response.ApplicationNoteResponse;
+import com.motnip.applicationtracker.model.Application;
+import com.motnip.applicationtracker.model.ApplicationStatus;
+import com.motnip.applicationtracker.service.ApplicationNoteService;
 import com.motnip.applicationtracker.service.JobApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping("/application")
 public class JobPostController {
 
+    private JobApplicationService applicationService;
+    private ApplicationNoteService serviceNote;
+
     @Autowired
-    private JobApplicationService service;
+    public JobPostController(JobApplicationService applicationService, ApplicationNoteService serviceNote) {
+        this.applicationService = applicationService;
+        this.serviceNote = serviceNote;
+    }
 
     @PostMapping
-    public Application addNewApplication(@RequestBody ApplicationRequest newApplication) {
-
-        Application.ApplicationBuilder newApplicationBuilder =  Application.builder()
-                .companyName(newApplication.companyName())
-                .description(newApplication.description())
-                .notes(newApplication.notes());
-        Optional.ofNullable(newApplication.applicationDate()).ifPresent(newApplicationBuilder::applicationDate);
-        return newApplicationBuilder.build();
+    public Application addNewApplication(@RequestBody ApplicationRequest applicationRequest) {
+        return applicationService.save(applicationRequest);
     }
 
-    @PatchMapping("/firstContact-date")
-    public Application recordFirstContact(ApplicationFirstContactRequest firstContact) {
-        return service.updateFirstContact(firstContact);
+    @PatchMapping("/{applicationId}/firstContact-date")
+    public Application recordFirstContact(@PathVariable Long applicationId, @RequestBody ApplicationFirstContactRequest firstContact) {
+        return applicationService.updateFirstContact(applicationId, firstContact);
     }
 
-    @PatchMapping("/status")
-    public Application updateApplication(ApplicationAdvancementUpdateRequest updateRequest) {
-        return service.updateAdvancement(updateRequest);
+    @PatchMapping("/{applicationId}/status")
+    public Application updateApplication(@PathVariable Long applicationId, @RequestParam ApplicationStatus newStatus) {
+        return applicationService.updateStatus(applicationId, newStatus);
+    }
+
+    /**
+     * Aggiorno le note e lo stato dell'applicazione.
+     * */
+    @PatchMapping("/{applicationId}/progress")
+    public Application updateApplication(@PathVariable Long applicationId, @RequestBody ApplicationProgressUpdateRequest progressUpdate) {
+        return applicationService.updateProgress(applicationId, progressUpdate);
+    }
+
+    @GetMapping
+    public List<ApplicationResponse> getAllApplication() {
+        return applicationService.getAllApplication();
+    }
+
+    @GetMapping("/{id}/notes")
+    public List<ApplicationNoteResponse> getApplicationNotes(@PathVariable(name = "id") Long applicationId) {
+        return serviceNote.getAllNotesByApplicationId(applicationId);
     }
 }
