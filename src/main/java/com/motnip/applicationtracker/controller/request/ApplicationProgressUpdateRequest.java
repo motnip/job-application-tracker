b3fd4620208643e6bdd589b440c5cd4efb554b7b
motnip <>
2025-05-28 16:43:45 +0200

    Rename to ApplicationProgressUpdateRequest
    From ApplicationAdvancementUpdateRequest

--- src/main/java/com/motnip/applicationtracker/controller/request/ApplicationProgressUpdateRequest.java
+++ src/main/java/com/motnip/applicationtracker/controller/request/ApplicationProgressUpdateRequest.java
@@ -2,7 +2,7 @@ package com.motnip.applicationtracker.controller.request;
 
 import com.motnip.applicationtracker.model.ApplicationStatus;
 
-public record ApplicationAdvancementUpdateRequest(
+public record ApplicationProgressUpdateRequest(
         String notes,
         ApplicationStatus newStatus){
 }
