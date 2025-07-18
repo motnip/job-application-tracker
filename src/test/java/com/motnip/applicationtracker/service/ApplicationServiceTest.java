ab768ace5edf0bc5a30c0fb6de4e630b4cbbc612
motnip <14219590+motnip@users.noreply.github.com>
2025-07-18 17:36:41 +0200

    Add test job application creation

--- src/test/java/com/motnip/applicationtracker/service/ApplicationServiceTest.java
+++ src/test/java/com/motnip/applicationtracker/service/ApplicationServiceTest.java
@@ -1,7 +1,9 @@
 package com.motnip.applicationtracker.service;
 
 import com.motnip.applicationtracker.controller.request.ApplicationFirstContactRequest;
+import com.motnip.applicationtracker.controller.request.ApplicationRequest;
 import com.motnip.applicationtracker.dto.ApplicationDTO;
+import com.motnip.applicationtracker.dto.ApplicationWithNotesDTO;
 import com.motnip.applicationtracker.exception.JobApplicationStateException;
 import com.motnip.applicationtracker.model.Application;
 import com.motnip.applicationtracker.model.ApplicationState;
@@ -9,17 +11,21 @@ import com.motnip.applicationtracker.repository.ApplicationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,11 +33,17 @@ class ApplicationServiceTest {
 
     private final long applicationId = 1;
     @InjectMocks
-    ApplicationService sut;
+    private ApplicationService sut;
 
     @Mock
-    ApplicationRepository repository;
+    private ApplicationRepository repository;
+    @Mock
+    private ApplicationNoteService applicationNoteService;
+
     private Application application;
+
+    @Captor
+    private ArgumentCaptor<Application> applicationArgumentCaptor;
 
     @BeforeEach
     void setUp() {
@@ -48,7 +60,58 @@ class ApplicationServiceTest {
     }
 
     @Test
-    void whenUpdateFirstContactThenUpdateFirstContatDateAndState() {
+    void testCreateNewApplicationAndNotes() {
+
+        ApplicationRequest request = ApplicationRequest.builder()
+                .companyName("Company Name")
+                .applicationDate(LocalDate.now())
+                .description("Job description and company description")
+                .note("Note about this application")
+                .build();
+
+
+        //when
+        when(repository.save(applicationArgumentCaptor.capture())).thenAnswer(invocation -> {
+            Application result = (Application) invocation.getArgument(0);
+            result.setId(applicationId);
+            result.setCreationDate(Instant.now());
+            result.setUpdateDate(Instant.now());
+            return result;
+        });
+
+        when(applicationNoteService.getAllNotesByApplicationId(eq(applicationId))).thenReturn(Collections.emptyList());
+
+        ApplicationWithNotesDTO applicationDTO = sut.save(request);
+
+        //then
+        Application actualApplication = applicationArgumentCaptor.getValue();
+        assertAll("actualApplication",
+                () -> assertEquals(1, actualApplication.getId()),
+                () -> assertEquals(request.companyName(), actualApplication.getCompanyName()),
+                () -> assertEquals(request.applicationDate(), actualApplication.getApplicationDate()),
+                () -> assertEquals(request.description(), actualApplication.getDescription()),
+                () -> assertNull(actualApplication.getFirstContactDate()),
+                () -> assertEquals(ApplicationState.WAITING, actualApplication.getState()),
+                () -> assertEquals(1, actualApplication.getNotes().size())
+        );
+
+        assertAll("applicationDto",
+                () -> assertEquals(1, applicationDTO.id()),
+                () -> assertEquals(request.companyName(), applicationDTO.companyName()),
+                () -> assertEquals(request.applicationDate(), applicationDTO.applicationDate()),
+                () -> assertEquals(request.description(), applicationDTO.description()),
+                () -> assertNull(applicationDTO.firstContactDate()),
+                () -> assertEquals(ApplicationState.WAITING, applicationDTO.status()),
+                () -> assertEquals(1, actualApplication.getNotes().size()),
+                () -> assertEquals(request.note(), actualApplication.getNotes().get(0).getText()),
+                () -> assertNotNull(applicationDTO.creationDate()),
+                () -> assertNull(applicationDTO.updateDate())
+        );
+
+    }
+
+    @Test
+    void whenUpdateFirstContactThenUpdateFirstContaCtDateAndState() {
 
         //given
         ApplicationFirstContactRequest request = new ApplicationFirstContactRequest(LocalDate.now(), ApplicationState.REJECTED);
